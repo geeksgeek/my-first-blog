cfe93c95ac01e6d8ffff8c12adbe505b6b38f40d
frank <dev.suh@gmail.com>
2017-01-02 23:01:44 +0900

    added login, comment

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,8 @@
 # admin.py
 
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 # 관리자 페이지에서 Post 모델을 보려면 이 부분을 등록해야 함.
 admin.site.register(Post)
+admin.site.register(Comment)
